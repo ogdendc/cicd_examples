005a77c1484b4b38df41d8ea58cdebe3b159e805
david.ogden@databricks.com <david.ogden@databricks.com>
2024-01-08 15:29:27 +0000

    testing reference to job parameter

--- job_parameter_test_part2.py
+++ job_parameter_test_part2.py
@@ -1,5 +1,12 @@
 # Databricks notebook source
-my_catalog = dbutils.jobs.taskValues.get(taskKey = "job_task_1", key = "catalog_name_global")
+#my_catalog = dbutils.jobs.taskValues.get(taskKey = "job_task_1", key = "catalog_name_global")
+
+# COMMAND ----------
+
+# defined as a job parameter via the Jobs UI
+# job_task_catalog_name
+
+my_catalog = dbutils.widgets.get('job_task_catalog_name')
 
 # COMMAND ----------
 
